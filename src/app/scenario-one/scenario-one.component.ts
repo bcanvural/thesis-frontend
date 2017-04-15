e96f0d4a15ced053e78520786dab61cc76684b99
Baris Can Vural <bcanvural@gmail.com>
2017-04-15 18:29:09 +0200

    Fix graph bug

--- src/app/scenario-one/scenario-one.component.ts
+++ src/app/scenario-one/scenario-one.component.ts
@@ -16,14 +16,9 @@ export class ScenarioOneComponent {
     jobid: number;
     method: string;
     skills: Array<Skill>;
-    readyToDraw = false;
+    displayGraph = false;
     ctxArr: Array<string> = ["mychart", "mychart2", "mychart3", "mychart4", "mychart5"];
-    myChart: MyChart;
-    myChart2: MyChart;
-    myChart3: MyChart;
-    myChart4: MyChart;
-    myChart5: MyChart;
-
+    charts: Array<MyChart> = [];
     constructor(private apiService: ApiService){}
 
   onJobChange(jobid: number): void {
@@ -43,31 +38,30 @@ export class ScenarioOneComponent {
 
   attemptToDraw(): void {
      if (this.method && this.jobid && (this.skills && this.skills.length == this.SKILL_NUM) ) {
-         console.log('drawing..');
          this.apiService.getGraphData(this.jobid, this.method, this.skills)
          .then(json => this.drawGraph(json))
          .catch(error => {
              console.log(error);
              return;
          })
-         this.readyToDraw = true;
+         this.displayGraph = true;
      } else {
-         this.readyToDraw = false;
+         this.displayGraph = false;
      }
   }
 
   drawGraph(json: any): void {
-      console.log(json);
       var labels = [];
       this.skills.forEach(skill => labels.push(skill.skillName));
       var jobid = json.job_sim.jobid;
-      var jobSimArr = [json.job_sim.cat_1_sim,  json.job_sim.cat_2_sim, json.job_sim.cat_3_sim, json.job_sim.cat_4_sim, json.job_sim.cat_5_sim]
-      var charts = [this.myChart, this.myChart2, this.myChart3, this.myChart4, this.myChart5]
+      var jobSimArr = [json.job_sim.cat_1_sim,  json.job_sim.cat_2_sim, json.job_sim.cat_3_sim, json.job_sim.cat_4_sim, json.job_sim.cat_5_sim];
       json.cv_similarities.forEach((obj, index) => {
-          console.log(obj);
-          console.log(index);
           var cvSimArr = [obj.cat_1_sim, obj.cat_2_sim, obj.cat_3_sim, obj.cat_4_sim, obj.cat_5_sim];
-          charts[index] = new MyChart(this.ctxArr[index], labels, `CV ${obj.cvid}`, cvSimArr, `Job ${jobid}`, jobSimArr);
+          if(this.charts.length < json.cv_similarities.length) {
+              this.charts[index] = new MyChart(this.ctxArr[index], labels, `CV ${obj.cvid}`, cvSimArr, `Job ${jobid}`, jobSimArr);
+          } else {
+              this.charts[index].redrawGraph(this.ctxArr[index], labels, `CV ${obj.cvid}`, cvSimArr, `Job ${jobid}`, jobSimArr);
+          }
       })
   }
 }
